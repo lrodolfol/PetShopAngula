d068491d29c181951a430261fda51b0a403bea2d
rodolfojsilva <rodolfo0ti@gmail.com>
2023-02-07 21:03:53 -0300

    routes for views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<b>isso vai mostrar em todas telas</b><router-outlet></router-outlet>'
+  //templateUrl: './app.component.html' //aqui funcionaria se eu apontasse para um arquivo html
 })
 export class AppComponent {
   title = 'petshop';
